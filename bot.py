1a07c899825211a75d3a6f599d3d02b83e3d7a48
Kai Piper <kai9987kai@gmail.com>
2023-05-30 21:25:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-from tkinter import Canvas
-
 class Bot:
-    def __init__(self, canvas: Canvas, x: int, y: int, health: int = 100):
+    def __init__(self, canvas: tk.Canvas, x: int, y: int, health: int = 100):
         self.canvas = canvas
         self.x = x
         self.y = y
         self.health = health
         self.shape = canvas.create_rectangle(x, y, x + 10, y + 10, fill='red')
 
-    def move(self, dx: int, dy: int):
+    def move(self, dx: int = 1, dy: int = 1):
         self.x += dx
         self.y += dy
         self.canvas.move(self.shape, dx, dy)
