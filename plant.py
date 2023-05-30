078bfb863a42a516624cbb357adec03e5076067b
Kai Piper <kai9987kai@gmail.com>
2023-05-30 21:04:40 +0100

    Update plant.py

--- plant.py
+++ plant.py
@@ -1,8 +1,12 @@
+from tkinter import Canvas
+
 class Plant:
-    def __init__(self, x, y, energy=100):
+    def __init__(self, canvas: Canvas, x: int, y: int, energy: int = 100):
+        self.canvas = canvas
         self.x = x
         self.y = y
         self.energy = energy
+        self.shape = canvas.create_oval(x, y, x + 10, y + 10, fill='green')
 
     def grow(self):
         self.energy += 10
