9e4912777bafbb3ab14cc35472d71d249b45ff39
Kai Piper <kai9987kai@gmail.com>
2023-05-30 21:11:38 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="envirogen",
+    version="0.1.0",
+    description="A Python package for creating 2D and 3D environments with bots, obstacles, and plants.",
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+)
