fb4fc885c118e519c8ebc9bb7036e592d24288a1
Kai Piper <kai9987kai@gmail.com>
2023-05-30 21:03:45 +0100

    Update obstacle.py

--- obstacle.py
+++ obstacle.py
@@ -1,5 +1,9 @@
+from tkinter import Canvas
+
 class Obstacle:
-    def __init__(self, x, y, size):
+    def __init__(self, canvas: Canvas, x: int, y: int, size: int):
+        self.canvas = canvas
         self.x = x
         self.y = y
         self.size = size
+        self.shape = canvas.create_rectangle(x, y, x + size, y + size, fill='blue')
