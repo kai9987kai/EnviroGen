a488be5c8a280d49b3021df1480ed5480a3c2a46
Kai Piper <kai9987kai@gmail.com>
2023-05-30 21:03:25 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,20 @@
+from tkinter import Canvas
+
 class Bot:
-    def __init__(self, x, y, health=100):
+    def __init__(self, canvas: Canvas, x: int, y: int, health: int = 100):
+        self.canvas = canvas
         self.x = x
         self.y = y
         self.health = health
+        self.shape = canvas.create_rectangle(x, y, x + 10, y + 10, fill='red')
 
-    def move(self, dx, dy):
+    def move(self, dx: int, dy: int):
         self.x += dx
         self.y += dy
+        self.canvas.move(self.shape, dx, dy)
 
-    def take_damage(self, amount):
+    def take_damage(self, amount: int):
         self.health -= amount
         if self.health <= 0:
             self.health = 0
-            print("Bot has been destroyed!")
+            self.canvas.delete(self.shape)
